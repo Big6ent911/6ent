9a746521cc95e8957f0fc3f94e3247d88400ded6
6ENT <bio@6ent-2.local>
2024-10-14 15:07:43 +0100

    next-app

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+// import Counter 
+import Counter from "./component/counter"
+import UserList from "./component/useEffect";
+
+export default function Home() {
+  return (
+    <div>
+      {/*<Counter />*/}
+      <UserList />
+    </div>
+  );
+}
---
+++ src/app/service/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function service() {
+  return (
+    <div className='bg-blue-500'>
+      service
+    </div>
+  )
+}
